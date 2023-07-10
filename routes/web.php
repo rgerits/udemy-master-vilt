934bb0a7a7f70a49b660b9c76c7c768fec0f1f47
Ronald Gerits <ronald.gerits@gmail.com>
2023-07-10 20:45:30 +0200

    Section 7 WIP

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
+|-------------------------------------------------\-------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
@@ -16,3 +17,5 @@ use App\Http\Controllers\IndexController;
 Route::get('/', [IndexController::class, 'index']);
 
 Route::get('/hello', [IndexController::class, 'show']);
+
+Route::resource('listing', ListingController::class);
