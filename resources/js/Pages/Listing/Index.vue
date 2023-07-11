c481e4e12bb01026646ccc0a7bf7c033486f2c66
Ronald Gerits <ronald.gerits@gmail.com>
2023-07-11 22:16:29 +0200

    Section 7 WIP 2

---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+    listings: Array,
+});
+</script>
+
+<template>
+    <div>Hello!</div>
+    <div v-for="listing in listings">
+        {{ listing.city }}
+    </div>
+</template>
